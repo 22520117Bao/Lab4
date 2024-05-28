573e9f44a1987da3f4ccbc063cdafe79561f8212
22520117Bao <162546070+22520117Bao@users.noreply.github.com>
2024-05-28 21:14:12 +0700

    Add files via upload

---
+++ Bai5/Bai5/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Bai5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txturl = new System.Windows.Forms.TextBox();
+            this.txtusername = new System.Windows.Forms.TextBox();
+            this.txtpass = new System.Windows.Forms.TextBox();
+            this.rtbtoken = new System.Windows.Forms.RichTextBox();
+            this.btlogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(5, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "URL";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(5, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Username:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(5, 84);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // txturl
+            // 
+            this.txturl.Location = new System.Drawing.Point(90, 9);
+            this.txturl.Name = "txturl";
+            this.txturl.Size = new System.Drawing.Size(369, 20);
+            this.txturl.TabIndex = 3;
+            this.txturl.Text = "https://nt106.uitiot.vn/auth/token";
+            // 
+            // txtusername
+            // 
+            this.txtusername.Location = new System.Drawing.Point(90, 42);
+            this.txtusername.Name = "txtusername";
+            this.txtusername.Size = new System.Drawing.Size(307, 20);
+            this.txtusername.TabIndex = 4;
+            // 
+            // txtpass
+            // 
+            this.txtpass.Location = new System.Drawing.Point(90, 81);
+            this.txtpass.Name = "txtpass";
+            this.txtpass.PasswordChar = '*';
+            this.txtpass.Size = new System.Drawing.Size(307, 20);
+            this.txtpass.TabIndex = 5;
+            // 
+            // rtbtoken
+            // 
+            this.rtbtoken.Location = new System.Drawing.Point(8, 107);
+            this.rtbtoken.Name = "rtbtoken";
+            this.rtbtoken.Size = new System.Drawing.Size(516, 167);
+            this.rtbtoken.TabIndex = 6;
+            this.rtbtoken.Text = "";
+            // 
+            // btlogin
+            // 
+            this.btlogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btlogin.Location = new System.Drawing.Point(403, 35);
+            this.btlogin.Name = "btlogin";
+            this.btlogin.Size = new System.Drawing.Size(86, 66);
+            this.btlogin.TabIndex = 7;
+            this.btlogin.Text = "Login";
+            this.btlogin.UseVisualStyleBackColor = true;
+            this.btlogin.Click += new System.EventHandler(this.btlogin_Click1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 285);
+            this.Controls.Add(this.btlogin);
+            this.Controls.Add(this.rtbtoken);
+            this.Controls.Add(this.txtpass);
+            this.Controls.Add(this.txtusername);
+            this.Controls.Add(this.txturl);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txturl;
+        private System.Windows.Forms.TextBox txtusername;
+        private System.Windows.Forms.TextBox txtpass;
+        private System.Windows.Forms.RichTextBox rtbtoken;
+        private System.Windows.Forms.Button btlogin;
+    }
+}
+
---
+++ Bai5/Bai5/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
+
+namespace Bai5
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+      
+
+        private void btlogin_Click1(object sender, EventArgs e)
+        {
+            btlogin_ClickAsync();
+        }
+
+        private async Task btlogin_ClickAsync()
+        {
+            await AsyncConnectToApi();
+        }
+
+        private async Task AsyncConnectToApi()
+        {
+            string url = txturl.Text;
+            string u_name = txtusername.Text;
+            string pass = txtpass.Text;
+
+
+            var content = new MultipartFormDataContent
+             {
+                { new StringContent(u_name), "username" },
+                { new StringContent(pass), "password" }
+             };
+
+
+            try
+            {
+                using (HttpClient client = new HttpClient())
+                {
+                    HttpResponseMessage res = await client.PostAsync(url, content);
+                    string res_content = await res.Content.ReadAsStringAsync();
+
+                    if(res.IsSuccessStatusCode)
+                    {
+                        var jres = JObject.Parse(res_content);
+                        string tokentype = jres["token_type"].ToString();
+                        string accesstoken = jres["access_token"].ToString();
+                        rtbtoken.Text = $"Bearer {accesstoken}";
+                        rtbtoken.Text += "\n\n\n\n" + "Đăng nhập thành công";  
+                    }
+                    else
+                    {
+                        var jres = JObject.Parse(res_content);
+                        string detail = jres["detail"].ToString();
+                        MessageBox.Show("Dang nhap that bai", "Error", MessageBoxButtons.OK);
+                    }
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Loi: { ex.Message}","Sai");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
